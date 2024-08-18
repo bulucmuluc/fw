f9712027d57a7f86c73bc468c80b3f752e85e19a
bulucmuluc <129214586+bulucmuluc@users.noreply.github.com>
2024-08-18 21:47:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,14 +9,13 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
 #    General Public License for more details.
 # 
-#    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License > .
+#    License can be found in < https://github.com/Ayush7445/telegram-auto_forwarder/blob/main/License >.
 
 # Import necessary modules
 from telethon import TelegramClient, events
 from decouple import config
 import logging
 from telethon.sessions import StringSession
-import os
 
 # Configure logging
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
@@ -45,6 +44,7 @@ try:
     steallootdealUser = TelegramClient(StringSession(SESSION), APP_ID, API_HASH)
     steallootdealUser.start()
 except Exception as ap:
+    logging.error(f"ERROR - {ap}")
     print(f"ERROR - {ap}")
     exit(1)
 
@@ -55,25 +55,31 @@ async def sender_bH(event):
         try:
             message_text = event.raw_text.lower()
 
+            # Check if the message contains any blocked text
             if any(blocked_text in message_text for blocked_text in BLOCKED_TEXTS):
-                print(f"Blocked message containing one of the specified texts: {event.raw_text}")
                 logging.warning(f"Blocked message containing one of the specified texts: {event.raw_text}")
+                print(f"Blocked message containing one of the specified texts: {event.raw_text}")
                 continue
 
+            # Handle media messages
             if event.media:
-                user_response = MEDIA_FORWARD_RESPONSE
-                if user_response != 'yes':
+                if MEDIA_FORWARD_RESPONSE != 'yes':
+                    logging.info(f"Media forwarding skipped by user for message: {event.raw_text}")
                     print(f"Media forwarding skipped by user for message: {event.raw_text}")
                     continue
 
                 await steallootdealUser.send_message(i, message_text, file=event.media)
+                logging.info(f"Forwarded media message to channel {i}")
                 print(f"Forwarded media message to channel {i}")
 
+            # Handle text-only messages
             else:
                 await steallootdealUser.send_message(i, message_text)
+                logging.info(f"Forwarded text message to channel {i}")
                 print(f"Forwarded text message to channel {i}")
 
         except Exception as e:
+            logging.error(f"Error forwarding message to channel {i}: {e}")
             print(f"Error forwarding message to channel {i}: {e}")
 
 # Run the bot
